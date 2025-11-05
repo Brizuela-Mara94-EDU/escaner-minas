e52782b3d00a4978cbf4116572e0bd32247b1307
Marcela Mancini <marcelamancinidiaz@gmail.com>
2025-11-05 19:11:27 -0300

    arreglalo ema jaja

--- vite.config.js
+++ vite.config.js
@@ -1,29 +1,29 @@
-import { defineConfig } from 'vite';
-import basicSsl from '@vitejs/plugin-basic-ssl';
-import { resolve } from 'path';
+import { defineConfig } from 'vite'
+import basicSsl from '@vitejs/plugin-basic-ssl'
+import { resolve } from 'path'
 
 export default defineConfig(({ command }) => {
-  if (command === 'serve') {
-    return {
-      plugins: [basicSsl()],
-      server: { host: true }
-    };
-  } else {
-    return {
-      base: '/AppMindAr/', // tu repo en GitHub Pages
-      build: {
-        outDir: 'docs',      // <- build final va a docs/
-        rollupOptions: {
-          input: {
-            main: resolve(__dirname, 'index.html'),
-            dino: resolve(__dirname, 'dino.html'),
-            minas: resolve(__dirname, 'minas.html'),
-            elements: resolve(__dirname, 'elements.html'),
-            scanner: resolve(__dirname, 'scanner.html'),
-            'arjs-scanner': resolve(__dirname, 'arjs-scanner.html')
-          }
-        }
-      }
-    };
+  const isDev = command === 'serve'
+  return {
+    plugins: [basicSsl()],
+    server: {
+      host: true,
+      open: true, // abre el navegador
+    },
+    base: isDev ? './' : '/AppMindAr/', // ✅ rutas correctas para ambos entornos
+    build: {
+      outDir: 'docs',
+      rollupOptions: {
+        input: {
+          main: resolve(__dirname, 'index.html'),
+          dino: resolve(__dirname, 'dino.html'),
+          minas: resolve(__dirname, 'minas.html'),
+          elements: resolve(__dirname, 'elements.html'),
+          scanner: resolve(__dirname, 'scanner.html'),
+          'arjs-dino': resolve(__dirname, 'arjs-dino.html'),
+          'arjs-scanner': resolve(__dirname, 'arjs-scanner.html'),
+        },
+      },
+    },
   }
-});
+})
