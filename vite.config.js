adedad7ed39efd5d864c4734d68797b95b29a4c3
Marcela Mancini <marcelamancinidiaz@gmail.com>
2025-11-28 10:12:26 -0300

    implementacion vista bio2

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import basicSsl from '@vitejs/plugin-basic-ssl'
 import { resolve } from 'path'
 
+
 export default defineConfig(({ command }) => {
   const isDev = command === 'serve'
   return {
@@ -16,12 +17,15 @@ export default defineConfig(({ command }) => {
       rollupOptions: {
         input: {
           main: resolve(__dirname, 'index.html'),
-          dino: resolve(__dirname, 'dino.html'),
           minas: resolve(__dirname, 'minas.html'),
           elements: resolve(__dirname, 'elements.html'),
           scanner: resolve(__dirname, 'scanner.html'),
-          'arjs-dino': resolve(__dirname, 'arjs-dino.html'),
-          'arjs-scanner': resolve(__dirname, 'arjs-scanner.html'),
+          bio: resolve(__dirname, 'bio.html'),
+          'dino-scanner': resolve(__dirname, 'dino-scanner.html'),
+          'arjs-scanner': resolve(__dirname, 'arjs-scanner.html'),     
+          'bio-scanner': resolve(__dirname, 'bio-scanner.html'),
+          'bio-elements': resolve(__dirname, 'bio-elements.html'),
+
         },
       },
     },
